f89db2b922cc4aba6642734823ba1135060c474b
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-01 17:54:47 +0530

    neww changes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var lastright = document.querySelector(".lastright")
 var lasttop = document.querySelector(".lasttop")
 var lastbottom = document.querySelector(".lastbottom")
 var nav = document.querySelector(".nav")
+var page1part1 =document.querySelector(".page1part1")
 
 var flag=0
 sun.addEventListener("click",function(){
@@ -19,6 +20,7 @@ sun.addEventListener("click",function(){
     lastright.style.color ="black"
     lasttop.style.color ="black"
     lastbottom.style.color ="black"
+    page1part1.style.backgroundColor ="whitesmoke"
     flag=1
 
     }else{
@@ -31,16 +33,17 @@ sun.addEventListener("click",function(){
     lasttop.style.color ="white"
     lastbottom.style.color ="white"
     sun.classList.add = "hidden"
+    page1part1.style.backgroundColor ="black"
         flag=0
     }
  
 })
 
 nav.addEventListener("mouseenter",function(){
-    cursor.style.backgroundColor="#3C3C3C"
+    cursor.classList.remove(".cursor")
 })
 nav.addEventListener("mouseleave",function(){
-    cursor.style.backgroundColor = "#FFC500"
+    cursor.classList.add(".cursor")
 })
 
 var cursor = document.querySelector(".cursor")
@@ -50,75 +53,243 @@ body.addEventListener("mousemove",function(dets){
     cursor.style.top =dets.y+"px"
 })
 
+var tl = gsap.timeline()
 
-
-
-gsap.from(".sweet",{
-    y:80,
-    delay:2,
-    duration:1.8,
+tl.from(".nav .navpart1",{
+    x:-200,
+    duration:2,
+    opacity:-1,
+})
+tl.from(".nav .navpart2",{
+    y:-20,
+    duration:2,
     opacity:0,
-    stagger:0.4
+
+},"-=1.8")
+tl.from(".nav .navpart3",{
+    y:-20,
+    duration:2,
+    opacity:0,
+
+},"-=1.1")
+
+
+gsap.to(".photoss img",{
+    transform:"translateX(100%)",
+    repeat:-1,
+    duration:3,
+    ease:"none"
+ })
+
+gsap.from(".leftimg",{
+              x:-130,
+            duration:1.8,
+            opacity:0,
+            stagger:0.4,
+            scrollTrigger:{
+                trigger:".leftimg",
+                scroller:".main",
+                
+                start:"top 70%",
+            }
 })
 
 gsap.from(".rightimg",{
-    y:80,
-    delay:2,
+    x:130,
     duration:1.8,
     opacity:0,
-    stagger:0.4
-})
-
-
-gsap.from(".leftimg",{
-    y:80,
-    delay:2,
-    duration:1.8,
-    opacity:0,
-    stagger:0.4
-})
-
-gsap.from(".start",{
-    y:80,
-    delay:2.1,
-    duration:1.9,
-    opacity:0,
-    stagger:0.4
-})
-gsap.from(".egallogo",{
-    delay:0.9,
-    duration:2,
-    opacity:-1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:".rightimg",
+        scroller:".main",
+        
+        start:"top 70%",
+    }
 })
 gsap.from(".aelovera",{
-    y:100,
-    duration:2,
-    delay:1,
-    opacity:-1,
-})
-gsap.from(".rexbox",{
-    y:80,
-    duration:3,
-    delay:3,opacity:0,
+        y:100,
+        duration:2,
+        opacity:0,
+        scrollTrigger:{
+            trigger:".aelovera",
+            scroller:".main",
+            
+            start:"top 85%"
+           
+        }
+    })
+    
+    gsap.from(".rexboxleft",{
+        x:-150,
+        duration:1.6,
+        opacity:0,
+        scrollTrigger:{
+            trigger:".rexboxleft",
+            scroller:".main",
+            
+            start:"top 85%"
+
+        }
+    })
+
+    gsap.from(".rexboxright",{
+        x:150,
+        duration:1.6,
+        opacity:0,
+        scrollTrigger:{
+            trigger:".rexboxright",
+            scroller:".main",
+            
+            start:"top 85%"
+
+        }
+    })
+
+    gsap.from(".cards .card1",{
+        x:200,
+        duration:2,
+        opacity:-1,
+        scrollTrigger:{
+            trigger:".cards .card1",
+            scroller:".main",
+            
+            start:"top 80%"
+        }
+    })
+    gsap.from(".cards .card2",{
+        y:200,
+        duration:1.6,
+        opacity:-1,
+        scrollTrigger:{
+            trigger:".cards .card2",
+            scroller:".main",
+            
+         start:"top 93%",
+        }
+
+    })
+    gsap.from(".cards .card3",{
+        x:-200,
+        duration:2,
+        opacity:-1,
+        scrollTrigger:{
+            trigger:".cards .card3",
+            scroller:".main",
+            
+            start:"top 70%"
+
+        }
+    })
+
+    gsap.from(".boxleft",{
+        x:-200,
+        opacity:0,
+        duration:2,
+        scrollTrigger:{
+            trigger:".boxleft",
+            scroller:".main",
+            
+            start:"top 60%"
+        }
+    })
+    gsap.from(".boxright",{
+        x:350,
+        opacity:0,
+        duration:2,
+        scrollTrigger:{
+            trigger:".boxright",
+            scroller:".main",
+            
+            start:"top 62%"
+        }
+    })
+
+    
+    gsap.from(".secondleftbox",{
+        x:-200,
+        opacity:0,
+        duration:2,
+        scrollTrigger:{
+            trigger:".secondleftbox",
+            scroller:".main",
+            
+            start:"top 62%"
+        }
+    })
+
+
+    gsap.from(".secondrightbox",{
+        x:200,
+        opacity:0,
+        duration:2,
+        scrollTrigger:{
+            trigger:".secondrightbox",
+            scroller:".main",
+            
+            start:"top 62%"
+        }
+    })
+
+
+    gsap.from(".lastbigbox",{
+        y:300,
+        opacity:0,
+        duration:3,
+        scrollTrigger:{
+            trigger:".lastbigbox",
+            scroller:".main",
+            
+
+        }
+    })
+
+
+gsap.to(".pageleftimg",{
+   transform:"translateY(-100%)",
+   duration:2,
+   repeat:-1,
+   ease:"none",
 
 })
-gsap.from(".rex",{
-    y:80,
-    duration:2,
-    delay:2,
-    opacity:0,
-    stagger:0.5
-})
-gsap.from(".rexxx",{
-    y:80,
+
+gsap.from(".pagerightimg",{
+    transform:"translateY(-100%)",
     duration:2.5,
-    delay:2,
-    opacity:0,
-    stagger:0.5
-})
-gsap.from(".cards",{
-    y:100,
-    duration:3,
-    opacity:0,
-    delay:4,
-})
+    repeat:-1,
+    ease:"none",
+ 
+ })
+
+        gsap.to(".forever h1",{
+            transform:"translateX(-100%)",
+            duration:3,
+            repeat:-1,
+            ease:"none",
+          })         
+    
+
+        //   if we want to edit nav bar so comment in this code
+ 
+        //   var fakenav = document.querySelector(".fakenav")
+ 
+        //   window.addEventListener("wheel",function(dets){
+        //     if(dets.deltaY>0){
+        //         console.log("seeda");
+
+        //         nav.classList.add("hidden")
+        //         nav.style= `ease duration-300`
+        //         fakenav.classList.add("hidden")
+
+        //     }else{
+        //         gsap.from(".nav",{
+        //             y:-30,
+        //             duration:0.5,
+        //             opacity:-1,
+        //         })
+        //         console.log("ulta");
+        //         nav.classList.remove("hidden")
+        //         fakenav.classList.remove("hidden")
+               
+        //     }
+
+        //   })
